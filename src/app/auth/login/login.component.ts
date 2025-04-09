c033f15cde001d027efb9bfca002b1edc0251273
DragoFiguer <figuer.pc@gmail.com>
2025-04-09 13:27:26 -0600

    Validacion login Reactivo

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +8,36 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  private formBuilder = inject(FormBuilder); //Inyeccion al formBuilder y Router para enlazar links
+  private router = inject(Router);
+  loginForm=this.formBuilder.group({ //Agregar los formsControls
+    email:['abc@gmail.com',[Validators.required, Validators.email]],
+    password:['',Validators.required],
+  }) // Objeto para el formReactive
+  
+
+  ngOnInit(): void {
+      
+  }
+
+  get email(){//Metodo get para la validacion reactiva
+    return this.loginForm.controls.email;
+  }
+
+  get password(){//Metodo get para la validacion reactiva
+    return this.loginForm.controls.password;
+  }
+
+  login(){ //Metodo Login valida la autenticidad
+    if(this.loginForm.valid){
+      console.log("Llamr al servicio de login");
+      this.router.navigateByUrl('/inicio');
+      this.loginForm.reset();
+    }else{
+      this.loginForm.markAllAsTouched();
+      alert("Error al ingresar los datos.");
+    }
+  }
 }
