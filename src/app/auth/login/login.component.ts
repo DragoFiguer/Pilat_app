ab91423b894544d03ddc96fde1bd90264d27fcd6
DragoFiguer <figuer.pc@gmail.com>
2025-04-11 00:26:34 -0600

    Creación de servicio Login e Interfaz

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, inject } from '@angular/core';
-import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/auth/login.service';
+import { LoginRequest } from '../../services/auth/loginRequest';
 
 @Component({
   selector: 'app-login',
@@ -8,34 +10,36 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
 
-  private formBuilder = inject(FormBuilder); //Inyeccion al formBuilder y Router para enlazar links
-  private router = inject(Router);
-  loginForm=this.formBuilder.group({ //Agregar los formsControls
-    email:['abc@gmail.com',[Validators.required, Validators.email]],
-    password:['',Validators.required],
+  private formBuilder = inject(FormBuilder); //Inyeccion al formBuilder
+  private router = inject(Router); //Inyeccion Router para enlazar links
+  private loginService = inject(LoginService); //Inyeccion de Servicio Login
+  loginForm = this.formBuilder.group({ //Agregar los formsControls
+    email: ['abc@gmail.com', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
   }) // Objeto para el formReactive
-  
+
 
   ngOnInit(): void {
-      
+
   }
 
-  get email(){//Metodo get para la validacion reactiva
+  get email() {//Metodo get para la validacion reactiva
     return this.loginForm.controls.email;
   }
 
-  get password(){//Metodo get para la validacion reactiva
+  get password() {//Metodo get para la validacion reactiva
     return this.loginForm.controls.password;
   }
 
-  login(){ //Metodo Login valida la autenticidad
-    if(this.loginForm.valid){
-      console.log("Llamr al servicio de login");
+  login() { //Metodo Login valida la autenticidad
+    if (this.loginForm.valid) {
+      //console.log("Llamr al servicio de login");
+      this.loginService.login(this.loginForm.value as LoginRequest);
       this.router.navigateByUrl('/inicio');
       this.loginForm.reset();
-    }else{
+    } else {
       this.loginForm.markAllAsTouched();
       alert("Error al ingresar los datos.");
     }
